fbd75612fa71456e91e3bb620519d6518e1b03be
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2023-10-19 10:22:10 +0700

    config dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle/ModeToggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-providers";
+import { cn } from "@/lib/utils";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -17,8 +19,19 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={openSans.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(openSans.className, "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            // forcedTheme="dark"
+            storageKey="discord-theme"
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
