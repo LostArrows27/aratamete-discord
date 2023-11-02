47b4c745767e62d53eb7a4910d50211c8fc9edfa
MinhMatMong <kurominhnhat@gmail.com>
2023-11-02 23:37:26 +0700

    add navigation side bar with create server modal

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ServerPage() {
+  return <div>Server Id</div>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-providers";
 import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-providers";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             storageKey="discord-theme"
             disableTransitionOnChange
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
