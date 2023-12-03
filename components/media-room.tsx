e50f1a342c55daf24c788187bb7f2ad073227907
MinhMatMong <kurominhnhat@gmail.com>
2023-12-03 15:25:28 +0700

    refix the video chat component

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -1,32 +1,23 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import {
-  LiveKitRoom,
-  VideoConference,
-  AudioConference,
-  ConnectionState,
-  ControlBar,
-  WidgetState,
-  useParticipants,
-  LayoutContextProvider,
-  ParticipantLoop,
-  ParticipantAudioTile,
-  Chat,
-} from "@livekit/components-react";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
 import "@livekit/components-styles";
 import { Channel } from "@prisma/client";
 import { useUser } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
-import { AudioUI } from "./audio";
 
 interface MediaRoomProps {
   chatId: string;
   video: boolean;
   audio: boolean;
-}
+};
 
-export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
+export const MediaRoom = ({
+  chatId,
+  video,
+  audio
+}: MediaRoomProps) => {
   const { user } = useUser();
   const [token, setToken] = useState("");
 
@@ -37,24 +28,26 @@ export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
 
     (async () => {
       try {
-        const resp = await fetch(
-          `/api/livekit?room=${chatId}&username=${name}`
-        );
+        const resp = await fetch(`/api/livekit?room=${chatId}&username=${name}`);
         const data = await resp.json();
         setToken(data.token);
       } catch (e) {
         console.log(e);
       }
-    })();
+    })()
   }, [user?.firstName, user?.lastName, chatId]);
 
   if (token === "") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
-        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
-        <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading...</p>
+        <Loader2
+          className="h-7 w-7 text-zinc-500 animate-spin my-4"
+        />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading...
+        </p>
       </div>
-    );
+    )
   }
 
   return (
@@ -66,18 +59,7 @@ export const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
       video={video}
       audio={audio}
     >
-      {/* {video ? <VideoConference /> :<AudioConference   /> } */}
-      {/* <ControlBar
-        variation="minimal"
-        controls={{
-          microphone: true,
-          screenShare: false,
-          camera: false,
-          chat: true,
-        }}
-      />
-      <ConnectionState /> */}
-      <AudioUI />
+      <VideoConference />
     </LiveKitRoom>
-  );
-};
+  )
+}
